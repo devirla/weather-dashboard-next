eee7665874f74d083f5c1042a77c0454c373ae40
AnnIrl <80989563+AnnIrl@users.noreply.github.com>
2025-05-12 23:00:52 +0200

    changes in files

--- lib/CitiesBox.tsx
+++ lib/CitiesBox.tsx
@@ -9,7 +9,7 @@ const CitiesBox = async ({ city }: { city: string }) => {
     return console.log(cities);
   };
 
-  cities.some((item) => item === city) ? changeCities(cities) : "";
+  //cities.some((item) => item === city) ? changeCities(cities) : cities;
 
   async function getWeather(city: string) {
     const res = await fetch(
@@ -25,7 +25,10 @@ const CitiesBox = async ({ city }: { city: string }) => {
     };
   }
 
-  const weatherData = await Promise.all(cities.map(getWeather));
+  const weatherData = async (cities: string[]) => {
+    cities.some((item) => item === city) ? changeCities(cities) : cities;
+    return await Promise.all(cities.map(getWeather));
+  };
 
   return (
     <div className="=w-full xl:w-[30vw]">
