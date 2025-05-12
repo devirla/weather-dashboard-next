58eb1cab0fb21b49764def36532417cb778a8140
AnnIrl <80989563+AnnIrl@users.noreply.github.com>
2025-05-12 15:29:04 +0200

    changes in files

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,8 @@ export default async function Home({
   const query = (await searchParams).query;
   let city: string = "";
 
-  {
-    query == undefined ? (city = "Warsaw") : (city = query);
-  }
+  query == undefined ? (city = "Warsaw") : (city = query);
+
   console.log("Query" + city);
 
   const session = await auth();
